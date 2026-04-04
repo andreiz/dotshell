2af47804fa6f6886d71b6245fad7edfef78ed65c
Andrei Zmievski <andrei@zmievski.org>
2026-04-04 11:02:20 -0700

    Auto-install antigen in zsh post_install hook

--- modules/zsh/module.sh
+++ modules/zsh/module.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
 post_install() {
+    # Install antigen if not present
+    if [[ "$DOTSHELL_OS" == "darwin" ]]; then
+        if ! brew list antigen &>/dev/null; then
+            info "Installing antigen via Homebrew..."
+            brew install antigen
+        fi
+    elif [[ "$DOTSHELL_OS" == "linux" ]]; then
+        if [[ ! -f /usr/share/zsh-antigen/antigen.zsh ]]; then
+            info "Installing antigen via apt..."
+            sudo apt install -y zsh-antigen
+        fi
+    fi
+
+    # Set zsh as default shell if needed
     if [[ "$SHELL" != */zsh ]]; then
         info "Changing default shell to zsh"
         chsh -s "$(which zsh)"
